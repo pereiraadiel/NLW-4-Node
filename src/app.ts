0cb4c8bb4ff213dcf3ad746301f95a5c0f9cccf4
Adiel Pereira <adielp16@gmail.com>
2021-02-22 11:53:01 -0300

    finalizada a aula 1

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-import express from 'express'
+import express from "express";
+import routes from "./routes";
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
-import app from './app'
+import app from "./app";
+import colors from "colors";
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log(colors.green("[OK] Server is running..."));
+});
