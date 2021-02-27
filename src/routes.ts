09f7f23f2479d562bbfb0ec166d9cd409bb372e4
Adiel Pereira <adielp16@gmail.com>
2021-02-27 12:01:24 -0300

    finalizada aula 2

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
+import { UserController } from "./controllers/UserController";
 
 const routes = Router();
+
+const userController = new UserController();
 
 routes.get("/", (req, res) => {
   return res.json({
     message: "Hello World!",
   });
 });
+
+routes.post("/users", userController.create)
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import "reflect-metadata";
 import app from "./app";
 import colors from "colors";
+
+import "./database";
 
 app.listen(3333, () => {
   console.log(colors.green("[OK] Server is running..."));
